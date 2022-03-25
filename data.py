68aee60124cfcc5eed9979fb5cb8fecaababbf06
Ali Mir <ali98mim@gmail.com>
2022-03-25 16:15:31 +0430

    Change private error message

--- data.py
+++ data.py
@@ -91,7 +91,7 @@ def make_graph(user_screen_name):
     # put_into_db(user)
     layer1 = getInteractions(user_screen_name.lower(), 4)[:30]
     if layer1 == 'private':
-        return {'error': 'The user is private.'}
+        return {'error': 'An error occurred. Try later.'}
     nodes = {user["id"]: {"screen_name": user_screen_name, "layer": 1}}
     edges = []
     for i, node_l1 in enumerate(layer1):
